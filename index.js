ca7658aaf938fe7275bda8a4eac66f61af3540c7
BenBarten91 <bba@fino.digital>
2019-10-12 23:47:12 +0200

    add babel support

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Koa = require('koa');
+import Koa from 'koa';
 const app = new Koa();
-app.use(async ctx => { ctx.body = 'Hello Docker World'; });
-app.listen(3000);
+app.use(async ctx => { ctx.body = 'Hello World of Docker'; });
+app.listen(8080);
