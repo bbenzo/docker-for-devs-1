6d80662de8bd4ecc47da5e6c8f641301eeb4a481
BenBarten91 <bba@fino.digital>
2019-10-25 12:17:32 +0200

    fix babel and docker setup

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM node:latest
 
 WORKDIR app
 
-COPY . /app
+COPY . .
 
-RUN npm install
+RUN npm install && npm run build
 
 EXPOSE 8080
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import Koa from 'koa';
+
 const app = new Koa();
+
 app.use(async ctx => { ctx.body = 'Hello World of Docker'; });
+
+console.log("Starting test server...");
+
 app.listen(8080);
